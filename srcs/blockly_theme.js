d14e7c08ca0f8a2dd8b8baf1a5f4db1a6d61fc5e
Murali Manohar Varma <67296473+mmv-dev@users.noreply.github.com>
2022-05-02 15:01:44 +0530

    new blocly

---
+++ srcs/blockly_theme.js
@@ -0,0 +1,59 @@
+Darker = Blockly.Theme.defineTheme("Darker", {
+  // base: Blockly.Themes.Zelos,
+  base: Blockly.Themes.Classic,
+
+  // Set default font colours
+
+
+  fontStyle: {
+    family: "Roboto, serif",
+    // weight: "bold",
+    size: 12,
+  },
+
+  // blockStyles: {
+  //   logic_blocks: {
+  //     colourPrimary: LOGIC_CATEGORY_COLOUR,
+  //   },
+  //   loop_blocks: {
+  //     colourPrimary: LOOP_CATEGORY_COLOUR,
+  //   },
+  //   text_blocks: {
+  //     colourPrimary: TEXT_CATEGORY_COLOUR,
+  //   },
+  //   math_blocks: {
+  //     colourPrimary: MATH_CATEGORY_COLOUR,
+  //   },
+  //   list_blocks: {
+  //     colourPrimary: LIST_CATEGORY_COLOUR,
+  //   },
+  //   colour_blocks: {
+  //     colourPrimary: COLOR_CATEGORY_COLOUR,
+  //   },
+
+  //   variable_blocks: {
+  //     colourPrimary: VARIABLE_CATEGORY_COLOUR,
+  //   },
+  //   procedure_blocks: {
+  //     colourPrimary: PROCEDURE_CATEGORY_COLOUR,
+  //   },
+  // },
+
+  componentStyles: {
+    workspaceBackgroundColour: "#0C0C32",
+    toolboxBackgroundColour: "#0C0C32",
+    toolboxForegroundColour: "#fff",
+    flyoutBackgroundColour: "#0C0C32",
+    flyoutForegroundColour: "#0C0C32",
+    flyoutOpacity: 0.6,
+    scrollbarColour: "#229DDB",
+    scrollbarOpacity: 0.2,
+    insertionMarkerColour: "#fff",
+    insertionMarkerOpacity: 0.3,
+    cursorColour: "#0C0C32",
+    // blackBackground: "#333",
+  },
+}).setBlockStyle("hat_blocks", {
+  colourPrimary: "#1551A4", colourSecondary: "#4280D7", colourTertiary: "#3373CC",
+  hat: "cap",
+}).setStartHats(startHats);
